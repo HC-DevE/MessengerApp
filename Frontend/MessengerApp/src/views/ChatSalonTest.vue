69e650086f6ce64328de778fede0922e80bb35b7
darkyHC <108454489+darkyHC@users.noreply.github.com>
2023-05-11 11:52:08 +0200

    add a template ex for vue socket-io

---
+++ Frontend/MessengerApp/src/views/ChatSalonTest.vue
@@ -0,0 +1,29 @@
+<template>
+    <!-- TODO: chat salon template html -->
+</template>
+  
+<script>
+import io from 'socket.io-client';
+
+export default {
+    data() {
+        return {
+            socket: null
+        }
+    },
+    mounted() {
+        this.socket = io('http://localhost:3000');
+        this.socket.on('connect', () => {
+            console.log('Connected to server');
+        });
+        this.socket.on('disconnect', () => {
+            console.log('Disconnected from server');
+        });
+    },
+    beforeDestroy() {
+        if (this.socket) {
+            this.socket.disconnect();
+        }
+    }
+}
+</script>  
